e26d31939cf175c546e38ce2a69290f9261201c7
ghillie575 <ghillie575@openspm.org>
2026-01-06 09:16:19 +0000

    Remove obsolete test files and installation scripts from the CodeQL build directory
    - Deleted progress.marks, CTestTestfile.cmake, and Makefile as they are no longer needed.
    - Removed cmake_install.cmake which contained installation instructions for tests.
    - Deleted the openspm_tests binary file as part of cleanup.
    - Removed the detected source root symlink.
    - Updated repository.yaml to include a URL for the testing repository.

--- _codeql_build_dir/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/Openspm/Openspm/tests
-# Build directory: /home/runner/work/Openspm/Openspm/_codeql_build_dir/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(openspm.unit "/home/runner/work/Openspm/Openspm/_codeql_build_dir/tests/openspm_tests")
-set_tests_properties(openspm.unit PROPERTIES  _BACKTRACE_TRIPLES "/home/runner/work/Openspm/Openspm/tests/CMakeLists.txt;18;add_test;/home/runner/work/Openspm/Openspm/tests/CMakeLists.txt;0;")
